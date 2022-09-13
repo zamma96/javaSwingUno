67540c6c236a366e2b72cc2de061b836ad0e6b2b
matte <matte@PCNacho>
2022-09-13 16:52:32 +0200

    First commit of 13/09, added all images in the resource folder, created User and Avatar classes,

---
+++ javaSwingUno/src/model/Avatar.java
@@ -0,0 +1,33 @@
+package model;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public class Avatar 
+{
+	private ImageIcon avatarIcon;
+	
+	//default avatar constructor
+	public Avatar()
+	{
+		Icon icon = new ImageIcon(".\\resources\\Avatars\\default.png");
+		this.avatarIcon = (ImageIcon) icon;
+	}
+	
+	//avatar constructor within a pull of avatar icons
+	public Avatar(int index)
+	{
+		this.avatarIcon = new ImageIcon(".\\resources\\Avatars\\" + "Avatar_" + index + ".png");
+	}
+	
+	public ImageIcon getAvatar()
+	{
+		return this.avatarIcon;
+	}
+	
+	public void setMaxLevelAvatar()
+	{
+		this.avatarIcon = new ImageIcon(".\\resources\\Avatars\\goldenAvatar.png");
+	}
+}
+
---
+++ javaSwingUno/src/model/User.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+public class User 
+{
+
+	private String nickName;
+	private Avatar avatar;
+	private Map<String, Integer> gamesPlayed;
+	private int level;
+	
+	//when building the user, the first int index has to be selected from 1 to 9 to select an avatar
+	public User(String nickName, int index, Map<String, Integer> gamesPlayed, int level)
+	{
+		this.setNickName(nickName);
+		this.avatar = new Avatar();
+		this.setGamesPlayed(gamesPlayed);
+		this.setLevel(level);
+	}
+
+	public String getNickName() 
+	{
+		return this.nickName;
+	}
+
+	public void setNickName(String nickName) 
+	{
+		this.nickName = nickName;
+	}
+
+	public Avatar getAvatar() 
+	{
+		return this.avatar;
+	}
+
+	public void setAvatar(int index) 
+	{
+		this.avatar = new Avatar(index);
+	}
+
+	public Map<String, Integer> getGamesPlayed() 
+	{
+		return this.gamesPlayed;
+	}
+
+	public void setGamesPlayed(Map<String, Integer> gamesPlayed) 
+	{
+		this.gamesPlayed = gamesPlayed;
+	}
+
+	public int getLevel() 
+	{
+		return this.level;
+	}
+
+	public void setLevel(int level) 
+	{
+		this.level = level;
+	}
+	
+	//Devo capire dove ficcare il metodo per gestire il livello dell'utente,
+	//capire se per le partite giocate serve una map o basta un'ArrayList
+	//public void setMaxLevelAvatar()
+	//{
+	//	if this.getUser().getLevel() == 10 then this.avatar = new Avatar().setMaxLevelAvatar();
+	//}
+}
